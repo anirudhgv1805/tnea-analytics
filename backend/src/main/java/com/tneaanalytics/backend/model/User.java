b5d751b59ae7b0e86e1827c23351c58eba81c6c7
anirudhgv1805 <anirudhgv04@gmail.com>
2026-05-15 15:44:57 +0530

    User model updated

--- backend/src/main/java/com/tneaanalytics/backend/model/User.java
+++ backend/src/main/java/com/tneaanalytics/backend/model/User.java
@@ -1,10 +1,34 @@
 package com.tneaanalytics.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    private String email;
 
 }
