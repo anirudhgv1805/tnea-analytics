2219698159afd92b91f82d213211c459053e0bd7
anirudhgv1805 <anirudhgv04@gmail.com>
2026-05-18 23:07:06 +0530

    added cross-origin

--- backend/src/main/java/com/tneaanalytics/backend/security/configurations/SecurityConfig.java
+++ backend/src/main/java/com/tneaanalytics/backend/security/configurations/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.tneaanalytics.backend.security.configurations;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +22,20 @@ public class SecurityConfig {
                 .formLogin(cust -> cust.disable());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
     }
 
 }
